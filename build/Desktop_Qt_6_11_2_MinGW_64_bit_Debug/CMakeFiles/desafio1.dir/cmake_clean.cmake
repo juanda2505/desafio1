5723cb6f30fe90b6475513298c60446b7444539a
Juan DavidA <juand.acevedom@udea.edu.co>
2026-09-14 08:09:51 -0500

    inicializacion del repo

---
+++ build/Desktop_Qt_6_11_2_MinGW_64_bit_Debug/CMakeFiles/desafio1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/desafio1.dir/main.cpp.obj"
+  "CMakeFiles/desafio1.dir/main.cpp.obj.d"
+  "desafio1.exe"
+  "desafio1.exe.manifest"
+  "desafio1.pdb"
+  "libdesafio1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/desafio1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Prueba de inicializacion del repo!" << endl;
+    return 0;
+}
